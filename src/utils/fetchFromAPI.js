0633b46596724eeb57c773d845932900190e74b9
Devansh Sharma <devanshsharma@Devanshs-MacBook-Air.local>
2022-10-11 22:37:20 +0530

    Video_card

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": 'a11b8642f7mshcf9abfd6bd9a3c6p1ef523jsn1a0ead3c9e3f',
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
